9d41022624b2dc5ab3454a4d9117a424eabc07e0
ysp <54001533+ysp313@users.noreply.github.com>
2020-11-23 10:49:08 +0100

    Initial commit

---
+++ src/main/java/fr/example/Heroku/HerokuApplication.java
@@ -0,0 +1,23 @@
+package fr.example.Heroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuApplication.class, args);
+	}
+
+}
+
+@RestController
+class HelloWorldController{
+	@GetMapping("/")
+	String hello(){
+		return "Hello World !";
+	}
+}
---
+++ src/test/java/fr/example/Heroku/HerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.example.Heroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
